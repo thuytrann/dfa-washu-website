eea1efa02075f595eb4d08ace0c8db52bfdb5a4d
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2021-04-04 17:36:00 -0500

    Implemented the content for the "Join DFA" page

--- page-about-us.php
+++ page-about-us.php
@@ -1,3 +1,117 @@
 <?php get_header(); ?>
+<html>
+    <head>
+       
+    </head> 
+    <style>
+    <?php include 'style.css' ?>
+    </style>
+    <body>
+        
+    <div class="join-DFA-box">
+        <img id="icons" src="img/two-sheets.png" alt="">
+        <div class=""> 
+        <h1>What does WashU DFA do?</h1>
+ 
+        <p> Design for America is a Design Research group that is part of a nationwide network of design thinking studios on college campuses.
+         We take a deep look into an area of interest co-defined with our community partners, using human-centered design as framework for evaluation and the generation of constructive insights.
+        </p>
+
+        <br> 
+        <p>
+        Projects usually run for a semester and are staffed by interdisciplinary student teams. As college students, we recognize we are often constrained in producing elaborate prototypes or implementation. 
+        Thus, the scope of our work is mostly limited to the generation of insights and ideas that can lend our community partners a more refined understanding of the focus area.
+        </p>
+        <br>
+        <p> 
+        Sometimes, a clear solution is realized where DFA is able to support implementation. In such cases, student teams  work more extensively on prototyping and implementation.
+        Such instances are more limited and often require multi-semester partnerships.
+        </p>
+        <br>
+        <a href="page-our-process.php">Our Process</a>
+        </div>
+    </div>
+
+
+    <div class="join-DFA-box">
+        <img id="icons" src="img/clock.png" alt="">
+        <div class="">
+        <h1>What is the time commitment?</h1>
+        <p>For new DFA members, the time commitment is Sunday studio (1.5 hours/week) and a brief new member meeting on Thursday nights (1 hour/week).</p>
+        </div>
+    </div>
+        
+    <div class="join-DFA-box">
+        <img id="icons" src="img/info.png" alt="">
+        <div class="">
+        <h1>What will I gain from DFA</h1>  
+        <p>Through DFA, you will learn the human centered design process and how to do design research. You will learn how to work with different organizations and critically and 
+        empathetically study a focus area. You will also learn a lot about the issues and topics that surround a given topic space that DFA works within. 
+        You will also get to work with a team of students from across WashU from many different backgrounds including design, engineering, business, and more. 
+        </p>
+        </div>
+     </div>
+
+        
+     <div class="join-DFA-box">
+        <img id="icons" src="img/person.png" alt="">
+        <div class="">
+        <h1>Who is in DFA</h1>
+        <p>DFA is an interdisciplinary club of undergraduate students from across WashU. Amongst our members are everyone from business students to art, architecture and engineering students. 
+            We are united by our interest in critically evaluating issues through a human-centered design lens. We believe in humble and informed engagement with the St. Louis community.
+         </p>
+         </div>
+     </div>
+         
+     <div class="join-DFA-box">
+        <img id="clipboard-img" src="img/clipboard.png" alt="">
+        <div class="">
+        <h1> How do I join?</h1>
+        <p>In order to join DFA, we first suggest that you attend an interest meeting at the beginning of the semester. In that meeting, we will go over the application process and 
+        talk a little bit more about what DFA does. The application process itself consists an online form and an interview with members of DFA. The goal is for us to get to know you and 
+        see if you will be a good fit for the organization. After the application process, you will begin new member training which lasts through your first semester in the organization.</p>
+        </div>
+     </div>
+
+     <div class="join-DFA-box">
+        <img id="icons" src="img/footprints.png" alt="">
+        <div class="">
+        <h1>What does the first semester of DFA look like?</h1>
+        <p>Your first DFA event will be a longer Bootcamp Studio, which introduces you to the club and the design research process we use when working in a topic space. 
+        This is longer than a usual studio and happens on a Sunday early in the semester. After Bootcamp, you will be put on a team that will be a little bit more guided than a 
+        team of experienced members. You will have a mentor and will likely be working in the same topic space as other new members. In addition to the normal Sunday studio, there are 
+        Thursday meetings that help build skills further in the project and sometimes the further development of your own project.</p>
+        </div>
+     </div>
+
+    <div class="join-DFA-box">
+        <img id="icons" src="img/earth.png" alt="">
+        <div class="">
+        <h1>What happens after the first semester of DFA?</h1>
+        <p>After your first semester, you have a little bit more flexibility in terms of what project you would like to do! You can go to an organization and scope a project for yourself,
+         whereas in the first DFA semester the project is scoped for you. You have greater freedom to pursue your own interests and work in topic spaces that you are passionate about.</p>
+      </div>
+     </div>
+
+    <div class="join-DFA-box">
+        <form action="index.php" method="POST">
+        <div id="application-form">
+            <h1>Application Form</h1>
+            First Name: <input type="text" name="firstName"> <br>
+            <br>
+            Last Name: <input type="text" name="lastName"> <br>
+            <br>
+            WUSTL Email: <input type="text" name="email">
+            <input type="submit">
+        </form>
+        </div>
+     </div>
+ 
+
+        </body>
+    
+
+
+</html>
 
 <?php get_footer(); ?>
